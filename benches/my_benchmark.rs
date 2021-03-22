cdd66aa662af92b3cdb8482dab161a965c7e07cc
Adam Chalmers <adam.s.chalmers@gmail.com>
2021-03-21 22:50:28 -0500

    Add more test inputs, for more expensive setters

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -5,23 +5,38 @@ const HEIGHT: usize = 200;
 
 fn compare_grid_impls(c: &mut Criterion) {
     let mut group = c.benchmark_group("Grid");
-    let operation = black_box(|Point { x, y }| x.pow(2) + y);
-    group.bench_function(BenchmarkId::new("1D Array", ""), |b| {
-        let mut g: array1d::Grid<usize, WIDTH, HEIGHT> = Default::default();
-        b.iter(|| g.set_all_parallel(operation));
+    let operation = black_box(|difficulty| {
+        move |Point { mut x, mut y }| {
+            for _ in 0..difficulty {
+                y = x + y;
+                x = if difficulty >= 1 && x % 2 == 0 {
+                    x.pow(2) + y
+                } else {
+                    x + y
+                }
+            }
+            x + y
+        }
     });
-    group.bench_function(BenchmarkId::new("2D Array", ""), |b| {
-        let mut g: array2d::Grid<usize, WIDTH, HEIGHT> = Default::default();
-        b.iter(|| g.set_all_parallel(operation));
-    });
-    group.bench_function(BenchmarkId::new("1D Vec", ""), |b| {
-        let mut g = vec1d::Grid::new(WIDTH, HEIGHT);
-        b.iter(|| g.set_all_parallel(operation));
-    });
-    group.bench_function(BenchmarkId::new("2D Vec", ""), |b| {
-        let mut g = vec2d::Grid::new(WIDTH, HEIGHT);
-        b.iter(|| g.set_all_parallel(operation));
-    });
+    let difficulties = vec![1, 5, 10];
+    for d in difficulties {
+        group.bench_with_input(BenchmarkId::new("1D Array", d), &d, |b, d| {
+            let mut g: array1d::Grid<usize, WIDTH, HEIGHT> = Default::default();
+            b.iter(|| g.set_all_parallel(operation(d.clone())));
+        });
+        group.bench_with_input(BenchmarkId::new("2D Array", d), &d, |b, d| {
+            let mut g: array2d::Grid<usize, WIDTH, HEIGHT> = Default::default();
+            b.iter(|| g.set_all_parallel(operation(d.clone())));
+        });
+        group.bench_with_input(BenchmarkId::new("1D Vec", d), &d, |b, d| {
+            let mut g = vec1d::Grid::new(WIDTH, HEIGHT);
+            b.iter(|| g.set_all_parallel(operation(d.clone())));
+        });
+        group.bench_with_input(BenchmarkId::new("2D Vec", d), &d, |b, d| {
+            let mut g = vec2d::Grid::new(WIDTH, HEIGHT);
+            b.iter(|| g.set_all_parallel(operation(d.clone())));
+        });
+    }
     group.finish();
 }
 
